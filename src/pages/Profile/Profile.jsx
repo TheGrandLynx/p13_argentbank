887fdbfe9e3069625328098567ad30058c16e8a7
TheGrandLynx <terence.ngama@gmail.com>
2024-02-08 09:23:11 +0100

    #4 Privacy Requirement

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -5,7 +5,7 @@ import User from "../../components/User/User";
 import Account from "../../components/Account/Account";
 import Header from "../../components/Header/Header";
 import Footer from "../../components/Footer/Footer";
-
+import accountData from "../../data/accountData.json";
 function UserProfile() {
   const token = useSelector((state) => state.auth.token);
   const dispatch = useDispatch();
@@ -57,21 +57,14 @@ function UserProfile() {
       <Header />
       <main className="bg-dark">
         <User firstname="Tony" lastname="Jarvis" />
-        <Account
-          title="Argent Bank Checking (x8349)"
-          amount="$2,082.79"
-          description="Available Balance"
-        />
-        <Account
-          title="Argent Bank Savings (x6712)"
-          amount="$10,928.42"
-          description="Available Balance"
-        />
-        <Account
-          title="Argent Bank Credit Card (x8349)"
-          amount="$184.30"
-          description="Current Balance"
-        />
+        {accountData.map((data) => (
+          <Account
+            key={data.id}
+            title={data.title}
+            amount={data.amount}
+            description={data.description}
+          />
+        ))}
       </main>
       <Footer />
     </div>
