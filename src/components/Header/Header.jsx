51756fa475b5f822b19dc65891978a728d61d8e1
TheGrandLynx <terence.ngama@gmail.com>
2024-02-08 09:06:32 +0100

    #3 Logout Requirement

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,20 @@
 import React from "react";
+import { useSelector, useDispatch } from "react-redux";
 import Logo from "../../assets/argentBankLogo.png";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { logout } from "../../redux/actions/auth.actions.js";
 
 function Header() {
+  const isConnected = useSelector((state) => state.auth.token);
+  const firstname = useSelector((state) => state.user.userData.firstname);
+
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const logoutHandler = () => {
+    dispatch(logout());
+    navigate("/");
+  };
   return (
     <header>
       <h1 className="sr-only">Argent Bank</h1>
@@ -10,10 +22,25 @@ function Header() {
         <Link to="/">
           <img src={Logo} alt="Bank Logo" />
         </Link>
-        <Link to="/Login">
-          <i className="fa-solid fa-circle-user"></i>
-          <p>Sign In</p>
-        </Link>
+        {isConnected ? (
+          <div className="connected">
+            <Link to="/profile">
+              <i className="fa-solid fa-2x fa-circle-user" />
+              <p>{firstname}</p>
+            </Link>
+            <Link to="/" onClick={logoutHandler}>
+              <i className="fa-solid fa-arrow-right-from-bracket" />
+              <p> Sign out </p>
+            </Link>
+          </div>
+        ) : (
+          <div className="not-connected">
+            <Link to="/login">
+              <i className="fa-solid fa-circle-user"></i>
+              <p>Sign In</p>
+            </Link>
+          </div>
+        )}
       </nav>
     </header>
   );
--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -1,10 +1,57 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { userProfile } from "../../redux/actions/user.actions.js";
 import User from "../../components/User/User";
 import Account from "../../components/Account/Account";
 import Header from "../../components/Header/Header";
 import Footer from "../../components/Footer/Footer";
 
 function UserProfile() {
+  const token = useSelector((state) => state.auth.token);
+  const dispatch = useDispatch();
+
+  /* Asynchronous function that retrieves user data and updates it with useEffect */
+  useEffect(() => {
+    if (token) {
+      const userData = async () => {
+        try {
+          const response = await fetch(
+            "http://localhost:3001/api/v1/user/profile",
+            {
+              method: "POST",
+              headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${token}`,
+              },
+            }
+          );
+          if (response.ok) {
+            const data = await response.json();
+            /* 
+                          Checking that the query response is indeed retrieved
+                          console.log(data) 
+                      */
+            const userData = {
+              createdAt: data.body.createdAt,
+              updatedAt: data.body.updatedAt,
+              id: data.body.id,
+              email: data.body.email,
+              firstname: data.body.firstName,
+              lastname: data.body.lastName,
+              username: data.body.userName,
+            };
+            /* Return user data in redux state */
+            dispatch(userProfile(userData));
+          } else {
+            console.log("error while retrieving profile");
+          }
+        } catch (error) {
+          console.error(error);
+        }
+      };
+      userData();
+    }
+  }, [dispatch, token]);
   return (
     <div className="profile-page">
       <Header />
